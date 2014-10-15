f63f8ef17546823cfb8ca8f45914e0a2bd1063fa
dansam100 <dansam100@gmail.com>
2014-10-15 02:38:26 -0400

    Cleaning up unused imports

--- app/src/main/java/com/ecg/complimenter/layout/ImageFlipperEventProvider.java
+++ app/src/main/java/com/ecg/complimenter/layout/ImageFlipperEventProvider.java
@@ -1,8 +1,5 @@
 package com.ecg.complimenter.layout;
 
-import android.content.Context;
-import android.view.View;
-
 public interface ImageFlipperEventProvider {
     public void setOnShareClickedEventListener(ImageFlipper.ImageFlipperListener listener);
     public void setOnFavoriteClickedEventListener(ImageFlipper.ImageFlipperListener listener);
